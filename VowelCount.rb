cd4ff1f9c7eb6dd48a09b64f6b5f26e0137a44c2
Justin Chi <j.chi2241@gmail.com>
2013-09-20 15:05:40 -0700

    add VowelCount

---
+++ VowelCount.rb
@@ -0,0 +1,10 @@
+def VowelCount(str)
+	vowels = %w{a e i o u A E I O U}
+	vowel_count = 0
+	str.split("").each do |char|
+		vowel_count += 1 if vowels.include?(char)
+	end
+	return vowel_count
+end
+
+p VowelCount("hello")
